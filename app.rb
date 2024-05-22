a6abd26e6d4dea8d3f66c5dc43a8b473c4334d1e
Waters <166139510+Waters-P@users.noreply.github.com>
2024-05-22 20:25:38 +0000

    scissors pag

--- app.rb
+++ app.rb
@@ -2,8 +2,44 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
+
   "
   <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+
   "
+  
+  erb(:mountains)
+  
+end
+
+
+
+get("/Play Scissors") do
+
+    @toolbox = ["scissors", "paper", "rock"]
+
+    @we = @toolbox[rand(0..2)]
+    @they = @toolbox[rand(0..2)]
+
+    @win = " Winner. They used #{@they} and we used #{@we}."
+    @lose = " Depricated. They used #{@they} and we used #{@we}."
+    @draw = " Tie. They used #{@they} and we used #{@we}."
+
+    erb(:scissors)
+
+
+end
+
+get("/Play Paper") do
+
+    @toolbox = ["paper", "rock", "scissors"]
+
+    @we = @toolbox[rand(0..2)]
+    @they = @toolbox[rand(0..2)]
+
+    @win = " Winner. They used #{@they} and we used #{@we}."
+    @lose = " Depricated. They used #{@they} and we used #{@we}."
+    @draw = " Tie. They used #{@they} and we used #{@we}."
+
+    erb(:paper)
 end
